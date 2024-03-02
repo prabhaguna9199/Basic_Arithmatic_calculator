7d6fe48f2077dad8984f9bbf395b3eee2483b1ae
JOTHIPRABHA PERIYASAMY <143201584+prabhaguna9199@users.noreply.github.com>
2024-03-02 13:02:11 +0530

    Add files via upload

---
+++ Arithmatic_operations.py
@@ -0,0 +1,29 @@
+class Arithmatic_operators:
+
+    def __init__(self, a, b):
+        # if a and b != float:
+        # raise TypeError("Values must be of type  float")
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+    def sub(self):
+        return self.a - self.b
+
+    def multiple(self):
+        return self.a * self.b
+
+    def divide(self):
+        if self.b != 0:
+            return self.a / self.b
+        else:
+            raise ZeroDivisionError("Cannot divide by zero")
+
+
+numbers = Arithmatic_operators(4, 8)
+print("Addition:", numbers.add())
+print("Subtraction:", numbers.sub())
+print("Multiplication:", numbers.multiple())
+print("Division:", numbers.divide())
---
+++ controller_2.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, HTTPException
+from Rest_api_.Arithmatic_operations import Arithmatic_operators
+from pydantic import BaseModel
+import uvicorn
+
+app = FastAPI()
+
+
+class Numbers(BaseModel):
+    a: float
+    b: float
+
+
+@app.post("/numbers/add")
+def addition(request: Numbers):
+    return {"result": Arithmatic_operators(a=request.a, b=request.b).add()}
+
+
+@app.post("/numbers/sub")
+def subraction(request: Numbers):
+    return {"result": Arithmatic_operators(a=request.a, b=request.b).sub()}
+
+
+@app.post("/numbers/multiplication")
+def multiplication(request: Numbers):
+    return {"result": Arithmatic_operators(a=request.a, b=request.b).multiple()}
+
+
+@app.post("/numbers/division")
+def division(request: Numbers):
+    try:
+        return {"result": Arithmatic_operators(a=request.a, b=request.b).divide()}
+    except ZeroDivisionError:
+        raise HTTPException(status_code=400, detail="cannot divide by zero")
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
